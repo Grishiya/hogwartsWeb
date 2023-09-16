cdb1513fd1b3d9051d15f0e505ad08b3a879412b
Grishiya <guruwolf8@gmail.com>
2023-09-17 00:42:28 +0300

    step 4.2

--- src/main/java/sky/pro/hogwartsWeb/controller/StudentController.java
+++ src/main/java/sky/pro/hogwartsWeb/controller/StudentController.java
@@ -1,4 +1,9 @@
 package sky.pro.hogwartsWeb.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
 }
