e2250cb5db45cd7c53e7cd9603ad57d65e34332a
Grisha <guruwolf8@gmail.com>
2023-09-23 17:23:02 +0300

    Добавил поиск студентов по факультету в sql запрос

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -4,3 +4,6 @@ select name from student;
 select * from student where lower("name") like '%о%';
 select *from student where age < 13;
 select *from student order by age ;
+select s.* from  student as s , faculty as f
+where s.faculty_id =f.id
+and faculty_id =2;
